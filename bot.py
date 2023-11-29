cf9dcce99ce642aabe1dcf9ac8effe733b3257bd
maleekmalik@gmail.com <maleekmalik@gmail.com>
2023-11-29 10:53:59 +0300

    Added log handler, error checker

--- bot.py
+++ bot.py
@@ -1,16 +1,32 @@
 import os
 import telebot
+import logging
+from dotenv import load_dotenv
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+# Enable logging
+# The logging module is used to log errors and debug messages
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+                    filename='bot.log', level=logging.INFO)
+
+# Get the bot token from the environment variables
+BOT_TOKEN = os.environ['BOT_TOKEN']
+if not BOT_TOKEN:
+    logging.error("Could not connect to Telegram API")
+    exit(1)
+
+# Create a bot instance
 bot = telebot.TeleBot(BOT_TOKEN)
 
-
-
+# Command handlers
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Welcome to Crypto Hub, where you can get information about "
                  "BlockChain and crypto currencies.\n\nYou can control me by "
-                 "sending the following commannds: "
+                 "sending the following commands: "
                  "\n /crypto - Get information about cryptocurrencies "
                  "\n /blockchain - Get information about blockchain")
 
@@ -36,33 +52,38 @@ def send_blockchain(message):
     bot.send_message(message.chat.id,"What do you want to know?:", reply_markup=markup)
 
 # Handle responses for the buttons
-
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-    if call.data == 'blockchain':
-        bot.send_message(call.message.chat.id, "Blockchain is a system of recording information in a way "
-                         "that makes it difficult or impossible to change, hack, or cheat the system. " 
-                         "A blockchain is essentially a digital ledger of transactions that is duplicated "
-                         "and distributed across the entire network of computer systems on the blockchain.")
-    elif call.data == 'how':
-        bot.send_message(call.message.chat.id, "Blockchain was invented by a person (or group of people) "
-                         "using the name Satoshi Nakamoto in 2008 to serve as the public transaction ledger of the cryptocurrency bitcoin. "
-                         "The invention of the blockchain for bitcoin made it the first digital currency to solve"
-                          " the double-spending problem without the need of a trusted authority or central server. "
-                          "The bitcoin design has inspired other applications.")
-    elif call.data == 'mining':
-        bot.send_message(call.message.chat.id, "Mining is the process of adding transaction records "
-                         "to Bitcoin's public ledger of past transactions (and a 'mining rig' "
-                         "is a colloquial metaphor for a single computer system that performs "
-                         "the necessary computations for 'mining'.")
-    elif call.data == 'smart':
-        bot.send_message(call.message.chat.id, "A smart contract is a computer program or a "
-                         "transaction protocol which is intended to automatically execute, "
-                         "control or document legally relevant events and actions according "
-                         "to the terms of a contract or an agreement.") 
-  
+    try:
+        if call.data == 'blockchain':
+            bot.send_message(call.message.chat.id, "Blockchain is a system of recording information in a way "
+                            "that makes it difficult or impossible to change, hack, or cheat the system. " 
+                            "A blockchain is essentially a digital ledger of transactions that is duplicated "
+                            "and distributed across the entire network of computer systems on the blockchain.")
+        elif call.data == 'how':
+            bot.send_message(call.message.chat.id, "Blockchain was invented by a person (or group of people) "
+                            "using the name Satoshi Nakamoto in 2008 to serve as the public transaction ledger of the cryptocurrency bitcoin. "
+                            "The invention of the blockchain for bitcoin made it the first digital currency to solve"
+                            " the double-spending problem without the need of a trusted authority or central server. "
+                            "The bitcoin design has inspired other applications.")
+        elif call.data == 'mining':
+            bot.send_message(call.message.chat.id, "Mining is the process of adding transaction records "
+                            "to Bitcoin's public ledger of past transactions (and a 'mining rig' "
+                            "is a colloquial metaphor for a single computer system that performs "
+                            "the necessary computations for 'mining'.")
+        elif call.data == 'smart':
+            bot.send_message(call.message.chat.id, "A smart contract is a computer program or a "
+                            "transaction protocol which is intended to automatically execute, "
+                            "control or document legally relevant events and actions according "
+                            "to the terms of a contract or an agreement.") 
+    except Exception as e:
+        logging.error(f"Error handling callback query: {str(e)}")
+
 def main():
-    bot.infinity_polling()
+    try:
+        bot.infinity_polling()
+    except Exception as e:
+        logging.error(f"Error polling: {str(e)}")
 
 if __name__ == '__main__':
     main()
