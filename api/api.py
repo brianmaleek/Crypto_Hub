bcfce0012e02ea1bf60b05eca465212e993b5220
washucode <estherw8525@gmail.com>
2023-12-16 21:10:55 +0300

    adjust price to accomodate queries with two currencies

--- api/api.py
+++ api/api.py
@@ -8,25 +8,24 @@ load_dotenv()
 
 API_KEY = os.environ['COIN_KEY']
 
-CRYPTO_URL =  'https://api.coingecko.com/api/v3/simple/'
+CRYPTO_URL =  'https://api.coingecko.com/api/v3/'
 
 # Get Crypto Price
 async def get_price(crypto):
     async with aiohttp.ClientSession() as session:
-        url = f'{CRYPTO_URL}/price?ids={crypto}&vs_currencies=usd'
+        url = f'{CRYPTO_URL}/simple/price?ids={crypto}&vs_currencies=usd'
         headers={'Authorization': f'Token {API_KEY}'}
         async with session.get(url, headers=headers) as resp:
             data = await resp.json()
-            return  data.get(crypto, ).get('usd')
+            return  data
         
 
 # Get Crypto Price History
-        
-async def get_price_history(crypto, days):
+async def get_price_history(crypto, date):
     async with aiohttp.ClientSession() as session:
-        url = f'{CRYPTO_URL}/coin/{crypto}/market_chart?vs_currency=usd&days={days}'
-        headers = {'Authorization': f'Token {API_KEY}'}
+    
+        url = f'{CRYPTO_URL}/coins/{crypto}/history?&date={date}&localization=eng'
+        headers={'Authorization': f'Token {API_KEY}'}
         async with session.get(url, headers=headers) as resp:
             data = await resp.json()
-            return data.get('prices')
-
+            return data
